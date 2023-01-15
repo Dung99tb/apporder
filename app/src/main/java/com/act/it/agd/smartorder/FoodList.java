41054d068807379af0c6ff52dcf5664137859edf
PHAM DUNG <dung.pham@pirago.work>
2023-01-15 15:57:31 +0700

    finish

--- app/src/main/java/com/act/it/agd/smartorder/FoodList.java
+++ app/src/main/java/com/act/it/agd/smartorder/FoodList.java
@@ -1,11 +1,15 @@
 package com.act.it.agd.smartorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,9 +17,15 @@ import com.act.it.agd.smartorder.Interface.ItemClickListener;
 import com.act.it.agd.smartorder.Model.Food;
 import com.act.it.agd.smartorder.ViewHolder.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodList extends AppCompatActivity {
 
@@ -25,6 +35,10 @@ public class FoodList extends AppCompatActivity {
     DatabaseReference foodList;
     String categoryId="";
     FirebaseRecyclerAdapter<Food, FoodViewHolder> adapter;
+    //Search Functionality
+    FirebaseRecyclerAdapter<Food, FoodViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +61,51 @@ public class FoodList extends AppCompatActivity {
         {
             loadListFood(categoryId);
         }
+        //Search
+//        searchView = (SearchView) findViewById(R.id.searchView);
+//        searchView.setQueryHint("Nhập tên món bạn muốn tìm");
+//        loadSuggest();//Write function to load Suggest from Firebase
+//        searchView.setSuggestionsAdapter(suggestList);
+//        searchView.addOnLayoutChangeListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                List<String> suggest = new ArrayList<>();
+//                for (String search:suggest)
+//                {
+//                    if (sea)
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
     }
+
+//    private void loadSuggest() {
+//        foodList.orderByChild("MenuId".equals(categoryId))
+//                .addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        for (DataSnapshot postSnapshot:dataSnapshot.getChildren())
+//                        {
+//                            Food item = postSnapshot.getValue(Food.class);
+//                            suggestList.add(item.getName());//Add name of food to suggest list
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                });
+//    }
 
     private void loadListFood(String categoryId) {
         adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(Food.class, R.layout.food_item, FoodViewHolder.class,
